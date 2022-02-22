c6014650e60ba5d78ddb6d5b40e588edcbc693d6
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-02-22 11:39:45 +0530

    Added Leap Year

---
+++ assignments/solutions/01-flow-of-program/leapYear.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const isLeapYear = (year) => {
+    return year % 4 === 0;
+};
+
+rl.question('Enter a year to check whether it is Leap Year or not: ', year => {
+    isNaN(Number(year)) ? console.log('Please enter a valid Year') :
+        console.log(`${year} is ${isLeapYear(year) ? '' : 'not '}a Leap Year`);
+    rl.close();
+});
+
+/**
+ * Q: Enter a year to check whether it is Leap Year or not: 2022
+ * A: 2022 is not a Leap Year
+ * 
+ * Q: Enter a year to check whether it is Leap Year or not: 2020
+ * A: 2020 is a Leap Year
+ */
