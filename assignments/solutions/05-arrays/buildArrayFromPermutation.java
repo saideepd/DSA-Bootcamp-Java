a26ad8cb2582bc242c6c766fa18aa5b147fad7f8
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-03-12 20:02:28 +0530

    Added 1920. Build Array from Permutation

--- assignments/solutions/05-arrays/buildArrayFromPermutation.java
+++ assignments/solutions/05-arrays/buildArrayFromPermutation.java
@@ -6,8 +6,8 @@
 public class buildArrayFromPermutation {
     public static void main(String[] args) {
         System.out.println("Build Array from Permutation");
-        int[] nums1 = {0,2,1,5,3,4};
-        int[] ans1 =  buildArray(nums1);
+        int[] nums1 = { 0, 2, 1, 5, 3, 4 };
+        int[] ans1 = buildArray(nums1);
         System.out.print("[0,2,1,5,3,4] : " + buildArray(nums1));
         for (int i : ans1) {
             System.out.print(i + " ");
@@ -15,10 +15,10 @@ public class buildArrayFromPermutation {
 
         System.out.println();
 
-        int[] nums2 = {5,0,1,2,3,4};
-        int[] ans2 =  buildArray(nums2);
+        int[] nums2 = { 5, 0, 1, 2, 3, 4 };
+        int[] ans2 = buildArray(nums2);
         System.out.print("[5,0,1,2,3,4] : ");
-        for(int i : ans2) {
+        for (int i : ans2) {
             System.out.print(i + " ");
         }
 
@@ -27,7 +27,7 @@ public class buildArrayFromPermutation {
 
     static int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             ans[i] = nums[nums[i]];
         }
         return ans;
@@ -38,6 +38,6 @@ public class buildArrayFromPermutation {
  * Output:
  * 
  * Build Array from Permutation
- * [0,2,1,5,3,4] : 0 1 2 4 5 3 
- * [5,0,1,2,3,4] : 4 5 0 1 2 3 
+ * [0,2,1,5,3,4] : 0 1 2 4 5 3
+ * [5,0,1,2,3,4] : 4 5 0 1 2 3
  */
