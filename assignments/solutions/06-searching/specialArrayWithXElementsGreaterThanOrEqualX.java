f5c466e7f4a6d17db45f648ce9547bdcfda5d947
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-04-09 01:36:35 +0530

    Added comments to 1608. Special Array with X Elements Greater than or Equal X

--- assignments/solutions/06-searching/specialArrayWithXElementsGreaterThanOrEqualX.java
+++ assignments/solutions/06-searching/specialArrayWithXElementsGreaterThanOrEqualX.java
@@ -20,14 +20,20 @@ public class specialArrayWithXElementsGreaterThanOrEqualX {
     }
 
     private static int specialArray(int[] nums) {
+        // Return if array length is 0
         if (nums.length == 0)
             return 0;
 
+        // Sort the array to find the x value
         Arrays.sort(nums);
 
         for (int x = 1; x <= nums.length; x++) {
+            // Define a target position in the array
             int target = nums.length - x;
 
+            // If value at target is >= x
+            // and target's previous value is less than x
+            // the we've found x
             if (nums[target] >= x && (target == 0 || nums[target - 1] < x))
                 return x;
         }
