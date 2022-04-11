d87d68759119d2aac5dd1311b807a7fb9ea364f2
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-04-12 01:45:22 +0530

    Fixed a condition in Sorted Matrix

--- assignments/solutions/06-searching/sortedMatrix.java
+++ assignments/solutions/06-searching/sortedMatrix.java
@@ -82,7 +82,7 @@ public class sortedMatrix {
             return binarySearch(matrix, rStart, 0, cMid - 1, target);
 
         // Search in 2nd half
-        if (target <= matrix[rStart][cMid + 1] && target <= matrix[rStart][cMid + 1])
+        if (target <= matrix[rStart][cMid + 1] && target <= matrix[rStart][cols - 1])
             return binarySearch(matrix, rStart, cMid + 1, cols - 1, target);
 
         // Search in 3rd half
