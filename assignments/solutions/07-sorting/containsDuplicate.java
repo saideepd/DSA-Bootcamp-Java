240c76403bc3becd86f987aec7ff74c3cd01ba75
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2022-04-16 01:09:44 +0530

    Changed Sorting to Ascending sort in Contains Duplicate

--- assignments/solutions/07-sorting/containsDuplicate.java
+++ assignments/solutions/07-sorting/containsDuplicate.java
@@ -53,7 +53,7 @@ public class containsDuplicate {
         for (int i = 0; i < nums.length - 1; i++) {
             swapped = false;
             for (int j = 1; j < nums.length - i; j++) {
-                if (nums[j] > nums[j - 1]) {
+                if (nums[j] < nums[j - 1]) {
                     int temp = nums[j];
                     nums[j] = nums[j - 1];
                     nums[j - 1] = temp;
